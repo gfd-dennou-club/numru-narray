5e705bfe20f4fd6675dd423245825252e6c0cff7
Seiya Nishizawa <seiya@gfd-dennou.org>
2016-05-09 11:07:34 +0900

    add check that NArray is loaded

--- lib/numru/narray.rb
+++ lib/numru/narray.rb
@@ -1,4 +1,7 @@
 unless ENV["OMP_NUM_THREADS"]
   ENV["OMP_NUM_THREADS"] = "1"
 end
+if defined?(NArray)
+  raise "NArray is alread loaded.\nNumRu::NArray cannot use with NArray.\n"
+end
 require "numru/narray/narray.so"
